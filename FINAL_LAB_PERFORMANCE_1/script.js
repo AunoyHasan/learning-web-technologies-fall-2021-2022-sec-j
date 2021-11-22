23e1a10c25d45f5c0f9fe999be559fd3e9edb388
Aunoy <hasanmahmudul2457@gmail.com>
2021-11-22 16:32:22 +0600

    3rd Commit

--- FINAL_LAB_PERFORMANCE_1/script.js
+++ FINAL_LAB_PERFORMANCE_1/script.js
@@ -3,21 +3,29 @@ function clear(){
 }
 
 function addition(){
+	let value1 = document.getElementById('d1');
+	let value2 = document.getElementById('d1');
+	return value1+value2;
 	
 }
 
 function subtraction(){
+	let value1 = document.getElementById('d1');
+	let value2 = document.getElementById('d1');
+	return value1-value2;
 	
 }
 
 function multiplication(){
+    let value1 = document.getElementById('d1');
+	let value2 = document.getElementById('d1');
+	return value1*value2;
 	
 }
 
 function division(){
-	
-}
-
-function result(){
+	let value1 = document.getElementById('d1');
+	let value2 = document.getElementById('d1');
+	return value1/value2;
 	
 }
