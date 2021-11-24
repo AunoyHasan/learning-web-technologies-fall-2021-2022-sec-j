0f74b194aa5984c043849ec3559c90eae6d3b313
Aunoy <hasanmahmudul2457@gmail.com>
2021-11-24 15:56:19 +0600

    3rd Commit

--- FINAL_LAB_PERFORMANCE_2/calculation.php
+++ FINAL_LAB_PERFORMANCE_2/calculation.php
@@ -2,8 +2,8 @@
 
 
     $equal = $_REQUEST['equal'];
-
-    $number = $_REQUEST['d1'];
+    $operator = $_REQUEST['operator'];
+    $number = $_REQUEST['value'];
    
 
     function addition(){
@@ -13,21 +13,44 @@
     }
 
     function deletion(){
-     $num1 = $number;
+        $num1 = $number;
         $num2 = $number;
         return $num1-$num2;
     }
 
     function multiplication(){
-     $num1 = $number;
+        $num1 = $number;
         $num2 = $number;
         return $num1*$num2;
     }
 
     function devition(){
-     $num1 = $number;
+        $num1 = $number;
         $num2 = $number;
          return $num1/$num2;
     }
+
+ 
+    switch ($operator) {
+    case '+':
+        $result = addition();
+        echo $result;
+        break;
+    case '-':
+        $result = subtraction();
+        echo $result;
+        break;
+    case '*':
+        $result = multiplication();
+        echo $result;
+        break;
+    case '/':
+        $result = divition();
+        echo $result;
+        break;
+    default:
+        echo 'Invalid Operator';
+        break;
+}
 
 ?>
--- FINAL_LAB_PERFORMANCE_2/script.js
+++ FINAL_LAB_PERFORMANCE_2/script.js
@@ -1,16 +1,11 @@
 function ajax(){
 
-    let value1 = document.getElementById('d1').value;
-    let value2 = document.getElementById('d1').value;
-    let value3 = document.getElementById('d1').value;
-    let value4 = document.getElementById('d1').value;
-    let value5 = document.getElementById('d1').value;
-    let value6 = document.getElementById('d1').value;
-    let value7 = document.getElementById('d1').value;
-    let value8 = document.getElementById('d1').value;
-    let value9 = document.getElementById('d1').value;
+    let value = document.getElementById('d1').value;
 
     let equal = document.getElementById('equal').value;
+
+    let operator = document.getElementById('operator').value;
+
     let xhttp= new XMLHttpRequest();
 
     xhttp.open('POST', 'calculation.php', true);
